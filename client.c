998fa91d75bebd80a63937676af13231e6cd48c1
Vishal Vishnani <vivi7647@colorado.edu>
2017-06-10 17:54:05 -0600

    Edited client.c t add encode/decode and send string

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+/*Includes*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,28 +9,56 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+
+/*Error function which takes message as arguement*/
 void error(char* msg){
   perror(msg);
   exit(0);
 }
 
+
+/*Function to decode packet*/
+void decode_packet(char *buffer){
+  int i;
+  for(i=0;i<12;i++){
+    *(buffer+i)=*(buffer+i)-5;
+  }
+}
+
+/*Function to analyse packet*/
+void analyse_packet(char *buffer){
+  if(*(buffer+5)=='1')
+    printf("\nVibration detected");
+  else
+    printf("\nNo Vibration");
+
+  if(*(buffer+11)=='1')
+    printf("   Touch Detected");
+  else
+    printf("   No touch");
+}
+
+
 int main(int argc , char *argv[]){
-  int sockfd,portno,n;
-  struct sockaddr_in serv_addr;
+  int sock,portno,n;
+  struct sockaddr_in server_addr;
   struct hostent *server;
-  char serverIp[]="10.0.0.42";
-  int buffer[256];
+  char serverIp[]="10.0.0.42"; //Ip address of raspberryPi
+  char buffer[100];
   
   if(argc<3){
     fprintf(stderr,"usage %s hostname port\n",argv[0]);
     exit(0);
   }
 
+  /*Take port number as command line arguement*/
   portno=atoi(argv[2]);
 
-  sockfd = socket(AF_INET,SOCK_STREAM, 0);
+  /*Create socket*/
+  sock = socket(AF_INET,SOCK_STREAM, 0);
   
-  if(sockfd<0){
+  /*If value returned is -1, display error*/
+  if(sock<0){
     error("socket failed");
   }
 
@@ -40,42 +69,43 @@ int main(int argc , char *argv[]){
     exit(0);
   }
 
-  bzero((char*)&serv_addr, sizeof(serv_addr));
+  bzero((char*)&server_addr, sizeof(server_addr));
 
-  serv_addr.sin_family=AF_INET;
+  server_addr.sin_family=AF_INET;
   
-  bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr,server->h_length);
+  bcopy((char*)server->h_addr, (char*)&server_addr.sin_addr.s_addr,server->h_length);
 
-  serv_addr.sin_port=htons(portno);
+  server_addr.sin_port=htons(portno);
   
-  
- if(connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr))<0){
+ /*connect*/
+ if(connect(sock,(struct sockaddr *)&server_addr, sizeof(server_addr))<0){
     error("connect failed");
   }
   
   while(1){
-    bzero(buffer,256);
-    n=read(sockfd,buffer,255);
+   
+    /*Zero out the elements of buffer at start*/
+    bzero(buffer,100);
+
+    /*Read the packet*/
+    n=read(sock,buffer,99);
   
+    /*If value returned is -1, display error*/
     if(n<0){
       error("Error reading from socket");
     } 
 
-    if(buffer[0]==1){
-      printf("\nTouch detected");
-    }
-    else{
-      printf("\nTouch Not DETECTED");
-    }
+    printf("\n\nString received: %s",buffer);
 
-    if(buffer[1]==1){
-      printf("\nVibration detected");
-    }
-    else{
-      printf("\nVibration Not DETECTED");
-    }
+    /*Decode the received packet*/
+    decode_packet((char*)&buffer);
+    printf("    Decoded Packet: %s",buffer);
 
-  }
+    /*Analyse packet*/
+    analyse_packet((char*)&buffer);
+  
+    }
   return 0;
+
 }
 
